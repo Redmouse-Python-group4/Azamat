b032d5a9e71df87dbde1954f0695a5612e1a854e
Syndicate7 <azamatksultanov@gmail.com>
2016-09-28 20:41:43 +0600

    added longestword prog

---
+++ longestword.py
@@ -0,0 +1,13 @@
+s = input('Введите строку разделенной пробелами, и программа выведет самое длинное слово:')
+maxlen = 0
+word = s.split(' ')
+for i in word:
+    if len(i) > maxlen:
+        maxlen = len(i)
+    else:
+        pass
+for i in word:
+    if len(i) == maxlen:
+        print("Самое длинное слово: %s" % i)
+    else:
+        pass
